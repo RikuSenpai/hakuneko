88671924cca64b11b9ec68ca5364f234ac68162b
Ronny Wegener <wegener.ronny@gmail.com>
2019-09-01 11:02:15 +0200

    [epikmanga] switched from promise syntax to async/await syntax

--- src/web/mjs/connectors/EpikManga.mjs
+++ src/web/mjs/connectors/EpikManga.mjs
@@ -11,63 +11,62 @@ export default class EpikManga extends Connector {
         this.url = 'https://www.epikmanga.com';
     }
 
-    _getMangaFromURI( uri ) {
-        let request = new Request( uri, this.requestOptions );
-        return this.fetchDOM( request, 'head title' )
-            .then( data => {
-                let id = uri.pathname;
-                let title = data[0].text.split(' | ')[0].trim();
-                return Promise.resolve( new Manga( this, id, title ) );
-            } );
+    async _getMangaFromURI(uri) {
+        try {
+            let request = new Request(uri, this.requestOptions);
+            let data = await this.fetchDOM(request, 'head title');
+            let id = uri.pathname;
+            let title = data[0].text.split(' | ')[0].trim();
+            return new Manga(this, id, title);
+        } catch( error ) {
+            return error;
+        }
     }
 
-    _getMangaList( callback ) {
-        let request = new Request( this.url + '/seri-listesi?type=text', this.requestOptions );
-        this.fetchDOM( request, 'div#pop-href div[id^=char-] a' )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.trim()
-                    };
-                } );
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangaList(callback) {
+        try {
+            let request = new Request(this.url + '/seri-listesi?type=text', this.requestOptions);
+            let data = await this.fetchDOM(request, 'div#pop-href div[id^=char-] a');
+            let mangaList = data.map(element => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                    title: element.text.trim()
+                };
+            });
+            callback(null, mangaList);
+        } catch(error) {
+            console.error(error, this);
+            callback(error, undefined);
+        }
     }
 
-    _getChapterList( manga, callback ) {
-        let request = new Request( this.url + manga.id, this.requestOptions );
-        this.fetchDOM( request, 'table.table tbody tr td:first-of-type a' )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.trim(),
-                        language: ''
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getChapterList(manga, callback) {
+        try {
+            let request = new Request(this.url + manga.id, this.requestOptions);
+            let data = await this.fetchDOM(request, 'table.table tbody tr td:first-of-type a');
+            let chapterList = data.map(element => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                    title: element.text.trim(),
+                    language: ''
+                };
+            });
+            callback(null, chapterList);
+        } catch(error) {
+            console.error(error, manga);
+            callback(error, undefined);
+        }
     }
 
-    _getPageList( manga, chapter, callback ) {
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        this.fetchDOM( request, 'div#reader div.chapter-content source.chapter-img' )
-            .then( data => {
-                let pageList = data.map( element => this.getAbsolutePath( element, request.url ) );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getPageList(manga, chapter, callback) {
+        try {
+            let request = new Request(this.url + chapter.id, this.requestOptions);
+            let data = await this.fetchDOM(request, 'div#reader div.chapter-content source.chapter-img');
+            let pageList = data.map(element => this.getAbsolutePath(element, request.url));
+            callback(null, pageList);
+        } catch(error) {
+            console.error(error, manga);
+            callback(error, undefined);
+        }
     }
 }
